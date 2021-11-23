c390916fa8886db25a32f0496b08bd46fe885b3e
Farheena14 <farheenaibrahim@gmail.com>
2021-11-23 09:16:39 +0530

    Middle header concept

--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-
-const route = require('./routes/route.js');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const route = require("./routes/route.js");
+const appMiddleware = require("./middlewares/mainMiddleware");
 
 const app = express();
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", route);
 
-
-const midGlb = function(req, res, next) {
-
-    console.log(new Date().toLocaleString())
-    console.log(req.ip)
-    console.log(req.originalUrl)
-    next()
-
-}
-app.use(midGlb)
-
-
-
-
-const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/Farheena_Ibrahim?retryWrites=true&w=majority", { useNewUrlParser: true })
-
-.then(() => console.log('mongodb running on 27017 and connected'))
-    .catch(err => console.log(err))
-
-app.use('/', route);
+mongoose
+    .connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/Farheena_Ibrahim?retryWrites=true&w=majority", { useNewUrlParser: true })
+    .then(() => console.log("mongodb running and connected"))
+    .catch((err) => console.log(err));
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+    console.log("Express app running on port " + (process.env.PORT || 3000));
 });
